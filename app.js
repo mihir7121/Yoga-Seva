c3ee4effe4e44416baff4de80e499e410f4d17ac
mihir7121 <mihir.nikam1@gmail.com>
2022-11-16 01:33:10 +0530

    setting up node app

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+const PORT = process.env.PORT || 3000
+
+app.use(cors());
+app.use(express.static(__dirname + '/assets'));
+app.use(bodyParser.json());
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+app.get('', function(req,res) {
+    res.render('index');
+});
+
+server.listen(PORT, function(err){
+    console.log(`Server is listening on http://localhost:${PORT}`)
+});
